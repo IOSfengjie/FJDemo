3f929b45c9c30fb92e8809b64314ed1e726743ef
873942808@qq.com <873942808@qq.com>
2016-11-10 08:53:27 +0800

    chushihua

---
+++ FengjieDemo/FengjieDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FengjieDemo
+//
+//  Created by fengjie on 2016/11/10.
+//  Copyright © 2016年 FengJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FengjieDemo/FengjieDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FengjieDemo
+//
+//  Created by fengjie on 2016/11/10.
+//  Copyright © 2016年 FengJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
